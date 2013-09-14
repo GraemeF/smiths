5c9a13b203ef9e87f33dba8ee3afa0600ee49226
Graeme Foster <graemef@gmail.com>
2013-09-14 17:15:10 +0100

    Use weighted event generators.

--- src/smiths/core.clj
+++ src/smiths/core.clj
@@ -5,15 +5,24 @@
 
 (def interval-between-events (minutes 1))
 
-(defn generate-device-event [device timestamp]
+(defn generate-application-added-event [device timestamp]
   {:event-type "Application added"
    :application-name "Orca"
    :executables ["C:\\Program Files\\Orca\\Orca.exe"]
    :deviceId (:id device)
    :timestamp timestamp})
 
+(def weighted-events
+  {generate-application-added-event 1})
+
+(defn create-weighted-generator [entry device timestamp]
+  (first {#((key entry) device timestamp) (val entry)}))
+
+(defn generate-weighted-event [device timestamp]
+  (weighted (map #(create-weighted-generator % device timestamp) weighted-events)))
+
 (defn generate-device-events [start interval]
-  (map #(generate-device-event {:id "dev1"} %) (periodic-seq start interval)))
+  (map #(generate-weighted-event {:id "dev1"} %) (periodic-seq start interval)))
 
 (defn -main []
   (println (generate-device-events (minus (now) (hours 1)) interval-between-events)))
