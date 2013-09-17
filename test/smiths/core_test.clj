6b84e9505e904223ec8db44d9a5b30b40c621397
Graeme Foster <graemef@gmail.com>
2013-09-17 19:16:12 +0100

    Add some more tests.

--- test/smiths/core_test.clj
+++ test/smiths/core_test.clj
@@ -4,22 +4,22 @@
 
 (facts "about `add-application-to-device`"
        (let [estate 
-             (add-application-to-device {:devices #{} 
-                                         :applications #{}
-                                         :instances #{}} #inst "2000")]
+             (add-application-to-device empty-estate #inst "2000")]
          (fact "it adds an application to the estate"
                (count (:applications estate)) => 1)
+         (fact "it adds a device to the estate"
+               (count (:devices estate)) => 1)
+         (fact "it adds an instance to the estate"
+               (count (:instances estate)) => 1)
          (fact "the added application looks like an application"
                (keys (first (:applications estate)))
-               => (contains #{:manufacturer
-                              :name
-                              :version}))))
-
-(facts "about `remove-application-from-device`"
-       (fact "it removes an instance from the estate"
-             (let [instance {:foo "bar"}]
-               (remove-application-from-device {:devices #{} 
-                                                :applications #{}
-                                                :instances #{instance}} 
-                                               #inst "2000"))
-             => #(empty? (:instances %))))
+               => (contains #{:manufacturer :name :version}))
+         (facts "adding another instance"
+                (fact "the estate contains 2 instances"
+                      (count (:instances (add-application-to-device estate #inst "2000")))
+                      => 2))
+         (facts "about `remove-application-from-device`"
+                (fact "it removes an instance from the estate"
+                      (remove-application-from-device estate 
+                                                      #inst "2000"))
+                => #(empty? (:instances %)))))
